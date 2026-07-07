0b59d168873cabd431e3afa8c700cfb11a7ebbd1
Scott Carnahan <128885296+ScottCarnahan@users.noreply.github.com>
2026-07-08 03:19:34 +0900

    More Golay API

--- VertexAlg/GroupTheory/Golay.lean
+++ VertexAlg/GroupTheory/Golay.lean
@@ -5,6 +5,7 @@ Authors: Scott Carnahan
 -/
 module
 
+public import Mathlib.LinearAlgebra.Matrix.GeneralLinearGroup.FinTwo
 public import Mathlib.LinearAlgebra.Projectivization.Action
 public import Mathlib.LinearAlgebra.Projectivization.Cardinality
 public import Mathlib.Algebra.Algebra.ZMod
@@ -56,23 +57,69 @@ lemma ofLine_apply_ne (x : K) :
     ofLine x ≠ Projectivization.mk K (fun i ↦ if i = 0 then 0 else 1)
       (Function.ne_iff.mpr (by use 1; simp)) := by
   simp [ofLine, mk_eq_mk_iff, funext_iff]
+/-
+lemma mem_ofLine_or (x : (ℙ K (Fin 2 → K))) :
+    x = Projectivization.mk K (fun i ↦ if i = 0 then 0 else 1)
+      (Function.ne_iff.mpr (by use 1; simp)) ∨ ∃ y, x = ofLine y := by
+  by_cases h : x = Projectivization.mk K (fun i ↦ if i = 0 then 0 else 1)
+      (Function.ne_iff.mpr (by use 1; simp))
+  · left
+    exact h
+  · right
 
+    sorry
+-/
 end Projectivization
 
 namespace Golay24
 open scoped LinearAlgebra.Projectivization
 open Projectivization
 
-instance : Fact (Nat.Prime 23) := by decide
+instance instFactPrimeOfNatNat : Fact (Nat.Prime 23) := by decide
 
-instance : Finite (ℙ (ZMod 23) (Fin 2 → ZMod 23)) :=
+instance instFiniteProjectivization : Finite (ℙ (ZMod 23) (Fin 2 → ZMod 23)) :=
   Nat.finite_of_card_ne_zero (by simp [Projectivization.card_of_finrank_two (ZMod 23)
     (Fin 2 → ZMod 23) (Module.finrank_fin_fun (ZMod 23))])
 
+/-- The Projective line is a fintype. (maybe do this computably?) -/
+noncomputable local instance instFintypeProjectivization :
+    Fintype (ℙ (ZMod 23) (Fin 2 → ZMod 23)) :=
+  Fintype.ofFinite (ℙ (ZMod 23) (Fin 2 → ZMod 23))
+/-
+instance : Compl (Finset (ℙ (ZMod 23) (Fin 2 → ZMod 23))) where
+  compl a :=
+
+noncomputable instance : DecidableEq (ℙ (ZMod 23) (Fin 2 → ZMod 23)) := by
+  intro a b
+  by_cases hi : a = Projectivization.mk (ZMod 23) (fun i ↦ if i = 0 then 0 else 1)
+      (Function.ne_iff.mpr (by use 1; simp))
+  · by_cases hj : b = Projectivization.mk (ZMod 23) (fun i ↦ if i = 0 then 0 else 1)
+      (Function.ne_iff.mpr (by use 1; simp))
+    · have : a = b := by rwa [← hj] at hi
+      exact isTrue this
+    · rw [← hi] at hj
+      exact isFalse <| Ne.symm hj
+  · sorry
+-/
 /-- The Golay codeword made of quadratic residues mod 23. -/
-def Q : Set (ℙ (ZMod 23) (Fin 2 → ZMod 23)) :=
-  {ofLine 0, ofLine 1, ofLine 2, ofLine 3, ofLine 4, ofLine 6, ofLine 8, ofLine 9, ofLine 12,
-    ofLine 13, ofLine 16, ofLine 18}
+def Q : Finset (ℙ (ZMod 23) (Fin 2 → ZMod 23)) :=
+  Finset.map ⟨ofLine, ofLine_injective⟩
+    ⟨[(0 : ZMod 23), 1, 2, 3, 4, 6, 8, 9, 12, 13, 16, 18], by decide⟩
+
+instance instSMulFinsetProjectivization {G K V : Type*} [AddCommGroup V] [DivisionRing K]
+    [Module K V] [DecidableEq (ℙ K V)] [Group G] [DistribMulAction G V] [SMulCommClass G K V] :
+    SMul G (Finset (ℙ K V)) :=
+  Finset.smulFinset
+
+/-- Make an element of a `ZMod 2` vector space from a finite subset. -/
+def ofFinset {ι : Type*} [DecidableEq ι] (s : Finset ι) : ι → ZMod 2 :=
+  fun a ↦ if a ∈ s then 1 else 0
+--#find_home! ofFinset --[Mathlib.Algebra.Field.ZMod]
+/-
+noncomputable def N (s : Finset (ZMod 23)) : (ℙ (ZMod 23) (Fin 2 → ZMod 23)) → ZMod 2 :=
+    ∑ i ∈ s, ofFinset (Matrix.GeneralLinearGroup.upperRightHom i • Qᶜ)
+-/
+
 
 /-- An equivalence from `Fin 24` to the projective line over `ZMod 23`. I could try to make this
 more general, but the correct approach is API for hyperplane complements. -/
@@ -101,8 +148,9 @@ noncomputable def toFin : Fin 24 ≃ ℙ (ZMod 23) (Fin 2 → ZMod 23) := by
             refine Fin.eq_of_val_eq ?_
             rw [ZMod.natCast_eq_natCast_iff] at this
             grind only [Nat.mod_eq_of_modEq]
-    · simp [Projectivization.card_of_finrank_two (ZMod 23)
-    (Fin 2 → ZMod 23) (Module.finrank_fin_fun (ZMod 23))]
-
+    · refine le_of_eq ?_
+      rw [Projectivization.card_of_finrank_two (ZMod 23)
+        (Fin 2 → ZMod 23) (Module.finrank_fin_fun (ZMod 23))]
+      norm_num
 
 end Golay24
